e4bd292f04f616d72d79e0c48c04ad3ff68e8248
iamjatinyadav <jatinyadav0000@gmail.com>
2023-09-25 18:00:17 +0530

    change gettext_lazy to gettext

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/user/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/user/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
 
 class UserAccountManager(BaseUserManager):
